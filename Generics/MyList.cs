0867ab43cf72443cb91848b86e3ef6ec08414197
nguyhai <tenth11@gmail.com>
2018-10-17 00:55:37 -0500

    Added in custom list and indexer

---
+++ Generics/MyList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MyList<T>
+    {
+        private T[] _items;
+
+        // Starts off like a property, with type T (since we are sorta making a list), and an index. 
+        //We return the _items array, and the element that is mapped to that index
+        public T this[int index] 
+        {
+            get
+            {
+                return _items[index];
+            }
+            set
+            {
+                _items[index] = value;
+            }
+        }
+
+
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public MyList()
+        {
+            _items = new T[2];
+            Capacity = 2;
+            Count = 0;
+        }
+
+        // We need an add method to populate our list. It needs to take generic arguments. The count is always the last index we are not using
+        public void Add(T item)
+        {
+            // If the capacity hits the count, a shallow copy of the array is created. The capacity is then increased by 2. the items in the array will equal new array. 
+            if (Capacity == Count)
+            {
+                T[]clone =(T[]) _items.Clone();
+                Capacity *= 2;
+                _items = new T[Capacity];
+
+                Array.Copy(clone, 0, _items, 0, clone.Length);
+            }
+            _items[Count] = item;
+            Count++;
+        }
+
+
+
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,40 +10,54 @@ namespace Generics
     {
         static void Main(string[] args)
         {
+            MyList<int> myIntList = new MyList<int>();
 
-            // Bascially, generic methods and classes can take multiple types
+            myIntList.Add(1);
+            myIntList.Add(1);
+            myIntList.Add(1);
+            myIntList.Add(1);
+            myIntList.Add(1);
 
-            int[] array = { 1, 4, 23, 5, 25, 315, 235, 235, 23, 52, 1, 412, 3 };
-            string[] stringArray = { "agasjkfs", "bsdfsdafasd", "csdfsafdsa", "dfafds" };
+            Console.WriteLine(myIntList.Capacity);
+            Console.WriteLine(myIntList.Count);
 
-            string[] sortedStringArray = Sort(stringArray);
-            int[] sortedIntArray = Sort(array);
+            Console.WriteLine(myIntList[0]); // You got an indexer added, so now we can get the number. Holy crap that is cool
 
-            Console.WriteLine(string.Join(", ", sortedIntArray));
-            Console.WriteLine(string.Join(", ", sortedStringArray));
+            
+
+            //// Bascially, generic methods and classes can take multiple types -----------------------------------
+
+            //int[] array = { 1, 4, 23, 5, 25, 315, 235, 235, 23, 52, 1, 412, 3 };
+            //string[] stringArray = { "agasjkfs", "bsdfsdafasd", "csdfsafdsa", "dfafds" };
+
+            //string[] sortedStringArray = Sort(stringArray);
+            //int[] sortedIntArray = Sort(array);
+
+            //Console.WriteLine(string.Join(", ", sortedIntArray));
+            //Console.WriteLine(string.Join(", ", sortedStringArray));
         }
 
-        public static T[] Sort<T>(T[] array) where T : IComparable<T>
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (array[i].CompareTo(array[j]) > 0 )
-                    {
-                        T temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-            return array;
-        }
+        //public static T[] Sort<T>(T[] array) where T : IComparable<T>
+        //{
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        for (int j = 0; j < array.Length; j++)
+        //        {
+        //            if (array[i].CompareTo(array[j]) > 0 )
+        //            {
+        //                T temp = array[i];
+        //                array[i] = array[j];
+        //                array[j] = temp;
+        //            }
+        //        }
+        //    }
+        //    return array;
+        //}
 
-        public static bool AreEqual<T>(T num1, T num2) where T:IComparable<T>
-        {
-            return num1.CompareTo(num2) == 0;
-        }
+        //public static bool AreEqual<T>(T num1, T num2) where T:IComparable<T>
+        //{
+        //    return num1.CompareTo(num2) == 0;
+        //}
 
 
     }
